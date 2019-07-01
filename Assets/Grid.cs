75454c7edec4dba964472d969bee757567448136
Jared Stokes <stokesjd@gmail.com>
2019-07-01 15:13:00 -0700

    first grid added

---
+++ Assets/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voxel
+{
+    public int X;
+    public int Y;
+    public int Z;
+
+    public Voxel(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        Debug.Log("New voxel created: " + x + "," + y + "," + z);
+    }
+}
